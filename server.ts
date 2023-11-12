7284f000fb78d94e977186f1b45cdc03b7408570
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-11-12 17:04:58 +0100

    server setup

---
+++ server.ts
@@ -0,0 +1,21 @@
+import 'express-async-errors';
+import { config, dbConnection, httpServer, initCron, logger } from './app';
+const { port, baseUrl, name, env } = config.app;
+
+const start = (): void => {
+  dbConnection()
+    .then((_connection: any): void => {
+      if (env !== 'test') {
+        httpServer.listen(port, (): void => {
+          logger.info(
+            `${name} server running: ${baseUrl}\n API documentation: ${baseUrl}/api-docs`,
+          );
+        });
+        initCron();
+      }
+    })
+    .catch((err: Error) => logger.error(err.message));
+};
+start();
+
+export default httpServer;
