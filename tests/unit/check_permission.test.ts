ca13e357ee5c82e1c146ee28fae942a1f3c16e14
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-11-13 11:58:19 +0100

    authorization validator test cases

---
+++ tests/unit/check_permission.test.ts
@@ -0,0 +1,14 @@
+import {checkPermissions} from '../../app/lib/utils';
+
+describe('CheckPermission', () => {
+	const reqUser = { name: 'Test User', id: 123, role: 'user' };
+
+	it('should  return unauthorized access with invalid resource user id', () => {
+		expect(() => checkPermissions(reqUser, 2)).toThrow()
+	});
+
+	it('should return true with valid resource user id', () => {
+
+		expect(checkPermissions(reqUser, 123)).toStrictEqual(true);
+	});
+});
